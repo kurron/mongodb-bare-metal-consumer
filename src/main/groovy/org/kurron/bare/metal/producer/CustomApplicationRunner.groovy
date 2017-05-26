409f5b93383bbbcc3f08633bea312859a962066b
Ron Kurr <kurr@kurron.org>
2017-05-26 09:24:17 -0400

    Minor clean up.

--- src/main/groovy/org/kurron/bare/metal/producer/CustomApplicationRunner.groovy
+++ src/main/groovy/org/kurron/bare/metal/producer/CustomApplicationRunner.groovy
@@ -9,8 +9,6 @@ import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 
-import java.util.concurrent.ThreadLocalRandom
-
 /**
  * Handles command-line arguments.
  */
@@ -29,32 +27,16 @@ class CustomApplicationRunner implements ApplicationRunner {
     @Autowired
     private ApplicationProperties theConfiguration
 
-    private static UUID generateModelID() {
-        UUID.randomUUID()
-    }
-
-    private static Date generateTimeStamp() {
-        Calendar.getInstance( TimeZone.getTimeZone('UTC') ).time
-    }
-
-    private static void randomize(byte[] buffer) {
-        ThreadLocalRandom.current().nextBytes(buffer)
-    }
-
-    private static Model createModel( byte[] payload ) {
-        new Model( primaryKey: generateModelID(), timestamp: generateTimeStamp(),  randomBytes: payload )
-    }
-
     @Override
     void run(ApplicationArguments arguments) {
 
         List<String> limit = Optional.ofNullable( arguments.getOptionValues('number-of-messages-to-read') ).orElse( ['100'] )
 
-        long start = System.currentTimeMillis()
-        def criteria = new Criteria()
-        def stream = theTemplate.stream( new Query( criteria ).limit( limit.first() as Integer ), Model )
         long totalBytes = 0
         int totalDocuments = 0
+
+        long start = System.currentTimeMillis()
+        def stream = theTemplate.stream( new Query( new Criteria() ).limit( limit.first() as Integer ), Model )
         def tracker = { Model model -> totalDocuments++ ; totalBytes += model.randomBytes.size() }
         stream.forEachRemaining( tracker )
         long stop = System.currentTimeMillis()
